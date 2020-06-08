f1b4d679e710b0c16367413ad4189d768d45f530
Patryk Stronski <p.stronski13@gmail.com>
2020-06-08 20:05:50 +0200

    working version

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -2,6 +2,7 @@ use crate::computer_turn::get_single_turn;
 use crate::points_calculation::calculate_field_points;
 use crate::user_turn::get_user_turn;
 use rand::Rng;
+use std::time::Instant;
 
 fn field_to_char(number: u8) -> String {
     if number == 0 {
@@ -34,19 +35,23 @@ fn print_board(board: &[[u8; 15]; 15]) {
     }
 }
 
-fn duel(board: &mut [[u8; 15]; 15]) -> u8 {
+fn duel(board: &mut [[u8; 15]; 15], mut computer_time: u128) -> u8 {
     print_board(board);
     let user_turn = get_user_turn();
     board[user_turn[0]][user_turn[1]] = 1;
     if calculate_field_points(board, user_turn, 1) >= 5 {
+        println!("Computer thinking time: {}", computer_time);
         return 1;
     }
+    let now = Instant::now();
     let computer_turn = get_single_turn(board);
+    computer_time += now.elapsed().as_millis();
     board[computer_turn[0]][computer_turn[1]] = 2;
     if calculate_field_points(board, computer_turn, 2) >= 5 {
+        println!("Computer thinking time: {}", computer_time);
         return 2;
     }
-    return duel(board);
+    return duel(board, computer_time);
 }
 
 pub fn start_new_game() {
@@ -56,7 +61,7 @@ pub fn start_new_game() {
     let starting_point_x = rand::thread_rng().gen_range(0, 15) as usize;
     let starting_point_y = rand::thread_rng().gen_range(0, 15) as usize;
     board[starting_point_x][starting_point_y] = computer_code;
-    let winner = duel(&mut board);
+    let winner = duel(&mut board, 0);
     print_board(&board);
     if winner == 1 {
         println!("YOU WON! CONGRATS!");
