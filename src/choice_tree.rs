71639d448aca9e7aca61dc8455e65fbe1da74698
Patryk Stronski <p.stronski13@gmail.com>
2020-06-08 21:39:49 +0200

    best opponents mov pick

--- src/choice_tree.rs
+++ src/choice_tree.rs
@@ -6,16 +6,21 @@ pub struct TreeSegment {
 }
 
 impl TreeSegment {
-    fn get_lowest_gain(&self) -> i8 {
-        let mut proposed_leaf_gain = 0i8;
+    fn get_best_opponent_move(&self) -> i8 {
+        let mut proposed_leaf_id = 0usize;
+        let mut best_leaf_gain = 0i8;
         for leaf_id in 0..self.leaves.len() {
             let leaf = &self.leaves[leaf_id];
-            let proposition = leaf.get_highest_gain();
-            if proposition < proposed_leaf_gain {
-                proposed_leaf_gain = proposition;
+            let proposition = leaf.gain;
+            if self.leaves[leaf_id].gain < best_leaf_gain {
+                best_leaf_gain = self.leaves[leaf_id].gain;
+                proposed_leaf_id = leaf_id;
+            }
+            if self.leaves[leaf_id].gain == -5 {
+                break;
             }
         }
-        return proposed_leaf_gain + self.gain;
+        return self.leaves[proposed_leaf_id].get_highest_gain();
     }
 
     pub fn get_highest_gain(&self) -> i8 {
@@ -23,7 +28,7 @@ impl TreeSegment {
             return self.gain;
         }
         if self.minimize_leaves {
-            return self.gain + self.get_lowest_gain();
+            return self.gain + self.get_best_opponent_move();
         }
         let mut proposed_leaf_gain = 0i8;
         for leaf_id in 0..self.leaves.len() {
